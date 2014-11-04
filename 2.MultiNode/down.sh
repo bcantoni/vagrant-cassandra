cdd536552198e2a0c00be76997ca7c407c9ec724
Brian Cantoni <brian@cantoni.org>
2014-11-03 22:12:23 -0800

    Add script for clean shutdown

---
+++ 2.MultiNode/down.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Vagrant Cassandra Project
+# Brian Cantoni
+#
+# Clean shutdown of each node (rather than just vagrant halting everything)
+
+for i in {0..3}
+do
+  echo Shutting down node$i
+  vagrant ssh node$i -c 'sudo shutdown -h now'
+done
+
+echo Done
